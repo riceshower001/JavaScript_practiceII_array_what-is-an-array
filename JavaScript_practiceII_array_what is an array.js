95f0cb785e3c166a6f80cf30ce3552e754fd5140
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-27 02:05:16 +0900

    Add files via upload

---
+++ JavaScript_practiceII_array_what is an array.js
@@ -0,0 +1,7 @@
+// Let's create an array.
+
+// Please define the constant "animals" and assign it an array whose elements are the strings "dog", "cat", "sheep".
+const animals = ["dog", "cat", "sheep"];
+
+// Please print the constant "animals".
+console.log(animals);
